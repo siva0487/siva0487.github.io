a1066c3ad0eb227599515251f17422ca86bbe2aa
Sivaram <sivaram@theretailinsights.com>
2020-01-24 21:48:48 +0530

    commitTheCode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import products from 'src/app/products.json';
+declare var $: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopping-cart';
+
+  public productsList: [] = products;
+
+  nCnt: number = 1;
+  price: any;
+  clickAdd(itemId, itemVal) {
+    var DataValue = $("#getQtyFromProductsPage-" + itemId);
+    this.nCnt = parseInt(itemVal) + 1;
+    DataValue.val(this.nCnt);
+    var theResCount = this.nCnt;
+    this.changeThePrice(itemId);
+  }
+  clickSub(itemId, itemVal) {
+    if (parseInt(itemVal) > 0) {
+      var DataValue = $("#getQtyFromProductsPage-" + itemId);
+      this.nCnt = parseInt(itemVal) - 1;
+      DataValue.val(this.nCnt);
+      var theResCount = this.nCnt;
+      this.changeThePrice(itemId);
+      if (theResCount == 0) {
+        DataValue.val(1);
+        this.changeThePrice(itemId);
+      }
+      else {
+        DataValue.val(theResCount);
+        this.changeThePrice(itemId);
+      }
+    }
+  }
+
+  changeThePrice(id)
+  {
+    var DataValue = $("#getQtyFromProductsPage-" + id).val();
+    var getPrice = $("#getExactPrice"+id).text();
+    this.price = parseInt(DataValue) * parseFloat(getPrice);
+    $("#theTotal"+id).text("Total:"+this.price);
+    console.log(this.price);
+  }
 }
